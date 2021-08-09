9302534659d3cd30d54544679f662ed6546f7330
xloz19 <tjwns53@naver.com>
2021-08-09 13:18:15 +0900

    last modify

--- js/weather.js
+++ js/weather.js
@@ -3,12 +3,12 @@ API_key = "d80e1765a89f0b1a221142fd3d5f9722";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
             const weatherContainer = document.querySelector("#weather span:first-child");
-            const city = document.querySelector("#weater span:last-child");
+            const city = document.querySelector("#weather span:last-child");
             city.innerText = data.name;
             weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
